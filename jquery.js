1fe8bc9ab6cb65210c9f741dabc7606166350722
Zack Fisch <zack@zfisch.com>
2014-07-09 10:21:41 -0400

    fixing wording

--- jquery.js
+++ jquery.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 				$('#move-number').html(++moveNumber);
 				board.push($(this).html());
 		};
-		/* in the array, we have a few possibilities for winning 
+		/* in the board, we have a few possibilities for winning 
 		0,1,2
 		3,4,5
 		6,7,8
@@ -30,7 +30,7 @@ $(document).ready(function(){
 		2,4,6
 		*/
 
-		
+
 	});
 
 });
