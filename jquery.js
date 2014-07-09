82aec8c3ca8f599847f6f1eca90029739b3e6b70
Zack Fisch <zack@zfisch.com>
2014-07-09 16:04:38 -0400

    Adding conditional branching to keep track of when someone has three in a row

--- jquery.js
+++ jquery.js
@@ -1,35 +1,104 @@
 $(document).ready(function(){
 
 	var moveNumber = 0;
-	window.board = [];
-
-	 
-
+	window.board = [];	 
 
 	$('td').click(function(){
 
 		if (moveNumber % 2 === 0 && $(this).html() === '' && moveNumber < 9) {
 				$(this).text('X');
 				$('#move-number').html(++moveNumber);
-				board.push($(this).html());
+
+				if ($(this).attr('id') === '0') {
+					board[0] = 'X'; 
+				}	else if ($(this).attr('id') === '1') {
+					board[1] = 'X'; 
+				} else if ($(this).attr('id') === '2') {
+					board[2] = 'X'; 
+				} else if ($(this).attr('id') === '3') {
+					board[3] = 'X'; 
+				}
+				else if ($(this).attr('id') === '4') {
+					board[4] = 'X'; 
+				}
+				else if ($(this).attr('id') === '5') {
+					board[5] = 'X'; 
+				}
+				else if ($(this).attr('id') === '6') {
+					board[6] = 'X'; 
+				}
+				else if ($(this).attr('id') === '7') {
+					board[7] = 'X'; 
+				}
+				else {
+					board[8] = 'X'; 
+				}
+
 		} else if (moveNumber % 2 === 1 && $(this).html() === '' && moveNumber < 9){
 				$(this).text('O');
 				$('#move-number').html(++moveNumber);
-				board.push($(this).html());
+
+				if ($(this).attr('id') === '0') {
+					board[0] = 'O'; 
+				}
+				else if ($(this).attr('id') === '1') {
+					board[1] = 'O'; 
+				}
+				else if ($(this).attr('id') === '2') {
+					board[2] = 'O'; 
+				}
+				else if ($(this).attr('id') === '3') {
+					board[3] = 'O'; 
+				}
+				else if ($(this).attr('id') === '4') {
+					board[4] = 'O'; 
+				}
+				else if ($(this).attr('id') === '5') {
+					board[5] = 'O'; 
+				}
+				else if ($(this).attr('id') === '6') {
+					board[6] = 'O'; 
+				}
+				else if ($(this).attr('id') === '7') {
+					board[7] = 'O'; 
+				}
+				else {
+					board[8] = 'O'; 
+				};
 		};
-		/* in the board, we have a few possibilities for winning 
-		0,1,2
-		3,4,5
-		6,7,8
 
-		0,3,6
-		1,4,7
-		2,5,8
-
-		0,4,8
-		2,4,6
-		*/
-
+		if (board[0] === board[1] && board[1] === board[2] && board[1] !== undefined) {
+			alert(board[0] + ' Wins!');
+			location.reload();
+		}
+		else if (board[3] === board[4] && board[4] === board[5] && board[4] !== undefined) {
+			alert(board[3] + ' Wins!');
+			location.reload();
+		}
+		else if (board[6] === board[7] && board[7] === board[8] && board[7] !== undefined) {
+			alert(board[6] + ' Wins!');
+			location.reload();
+		}
+		else if (board[0] === board[3] && board[3] === board[6] && board[3] !== undefined) {
+			alert(board[0] + ' Wins!');
+			location.reload();
+		}
+		else if (board[1] === board[4] && board[4] === board[7] && board[1] !== undefined) {
+			alert(board[1] + ' Wins!');
+			location.reload();
+		}
+		else if (board[2] === board[5] && board[5] === board[8] && board[2] !== undefined) {
+			alert(board[2] + ' Wins!');
+			location.reload();
+		}
+		else if (board[0] === board[4] && board[4] === board[8] && board[0] !== undefined) {
+			alert(board[0] + ' Wins!');
+			location.reload();
+		}
+		else if (board[2] === board[4] && board[4] === board[6] && board[4] !== undefined) {
+			alert(board[2] + ' Wins!');
+			location.reload();
+		}
 
 	});
 
